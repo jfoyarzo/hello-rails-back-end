adc18f03ea6d5262eb92fcd736ed94ea23bd56d3
jfoyarzo <foyarzo621@gmail.com>
2023-03-07 23:00:43 -0300

    Fix linter errors

--- test/controllers/api_controller_test.rb
+++ test/controllers/api_controller_test.rb
@@ -1,7 +1,7 @@
-require "test_helper"
+require 'test_helper'
 
 class ApiControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get api_index_url
     assert_response :success
   end
